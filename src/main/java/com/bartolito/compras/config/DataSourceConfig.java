e3e9cfc2db9f2842b00a8e14f29b56733858a590
AaronCueva <aaron.cueva21@gmail.com>
2025-11-10 19:41:31 -0500

    CAMBIOS BARTOLITO COMPRAS

---
+++ src/main/java/com/bartolito/compras/config/DataSourceConfig.java
@@ -0,0 +1,69 @@
+package com.bartolito.compras.config;
+
+
+import javax.sql.DataSource;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableJpaRepositories("com.ejemplo.jwtlogin")
+@EntityScan("com.ejemplo.jwtlogin")
+@EnableScheduling
+public class DataSourceConfig {
+
+    // =================== BD LOLFAR (Principal) ===================
+    @Bean
+    @ConfigurationProperties("spring.datasource.lolfar")
+    public DataSourceProperties lolfarDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "lolfarDataSource")
+    public DataSource lolfarDataSource() {
+        return lolfarDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "lolfarJdbcTemplate")
+    public JdbcTemplate lolfarJdbcTemplate(@Qualifier("lolfarDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+    // =================== BD SIGOLDB (Secundaria) ===================
+    @Bean
+    @ConfigurationProperties("spring.datasource.bi")
+    public DataSourceProperties sigoldbDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "biDataSource")
+    public DataSource biDataSource() {
+        return sigoldbDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "biJdbcTemplate")
+    public JdbcTemplate biDataSource(@Qualifier("biDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+
+    // 🔹 Mapper global
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
+}
---
+++ src/main/java/com/bartolito/compras/dto/rotacionProductos/TipoRentabilidadResponse.java
@@ -0,0 +1,20 @@
+package com.bartolito.compras.dto.rotacionProductos;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TipoRentabilidadResponse implements Serializable {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = -4337985194918759932L;
+	
+	private String categvta; 
+
+}
